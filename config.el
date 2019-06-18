c8086bb0d97533fcbc7938d9a589753650638b3c
Kushan Zaveri <kushanzaveri99@gmail.com>
2019-06-17 22:31:12 -0400

    init

---
+++ config.el
@@ -0,0 +1,30 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(setq package-enable-at-startup nil)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (eval-when-compile (require 'use-package)))
+
+(require 'use-package)
+(setq 
+	      use-package-always-defer t
+	      use-package-always-ensure t)
+
+(use-package spacemacs-theme
+  :init (load-theme 'spacemacs-dark t))
+
+(use-package evil)
+(evil-mode t)
+
+(use-package powerline
+	:init (powerline-default-theme))
+
+(use-package flycheck
+:init (global-flycheck-mode))
+
+(use-package ace-window
+:init (global-set-key (kbd "M-o") 'ace-window))
