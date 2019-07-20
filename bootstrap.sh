d17a90b1754af0503da733339fe9c4b321f641ac
kushan zaveri <kushanzaveri99@gmail.com>
2019-07-20 15:03:05 -0400

    Made changes (#5)

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+ZSH="$HOME/dotfiles/oh-my-zsh" sh install.sh
+#rm ~/.zshrc
+#ln -s ~/dotfiles/.zshrc ~/.zshrc
+#rm ~/.emacs
+#ln -s ~/dotfiles/.emacs ~/.emacs
